c4f45fac0fcd58b292aaf9560e7ee42f3c717248
Robbie Lane <lane.robbie@gmail.com>
2015-12-29 00:21:53 -0600

    gallery show and index

--- app/controllers/galleries_controller.rb
+++ app/controllers/galleries_controller.rb
@@ -1,4 +1,10 @@
 class GalleriesController < ApplicationController
-
+  def index
+    @galleries = Gallery.all
+  end
   
+  def show
+    @gallery = Gallery.find(params[:id])
+  end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
   root to: 'home#index'
   resources :pictures, only: [:show, :new, :create]
-  resources :galleries
+  resources :galleries, only: [:show, :index]
 end
