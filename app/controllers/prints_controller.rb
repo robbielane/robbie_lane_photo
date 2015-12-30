1917375594d00a4cc6f4f611f5cc7983f609a639
Robbie Lane <lane.robbie@gmail.com>
2015-12-30 08:36:57 -0600

    print show page

--- app/controllers/prints_controller.rb
+++ app/controllers/prints_controller.rb
@@ -2,4 +2,8 @@ class PrintsController < ApplicationController
   def index
     @prints = Picture.prints
   end
+
+  def show
+    @print = Picture.find(params[:id])
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -3,8 +3,19 @@ Rails.application.routes.draw do
   resources :pictures, except: [:index]
   resources :galleries
   get '/prints', to: 'prints#index'
+  get '/prints/:id', as: :print, to: 'prints#show'
   get '/login', to: 'sessions#new'
   post '/login', to: 'sessions#create'
   delete '/logout', to: 'sessions#destroy'
   get '/admin', to: 'admin#index'
+
+
+  # API
+  namespace :api do
+    namespace :v1 do
+      resources :galleries, only: [:index, :show]
+      get '/prints,' to: 'prints#index'
+      get '/recent', to: 'recent#index'
+    end
+  end
 end
