3a351cd7dac1e257832e0deea13e18730eef8afd
Robbie Lane <lane.robbie@gmail.com>
2015-12-29 18:44:53 -0600

    fixed image resolution prob

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -6,4 +6,12 @@ module ApplicationHelper
       'placeholder.jpg'
     end
   end
+
+  def gallery_cover_h(gallery)
+    if gallery.pictures.first
+      gallery.pictures.first.image.url(:h_thumb)
+    else
+      'placeholder.jpg'
+    end
+  end
 end
