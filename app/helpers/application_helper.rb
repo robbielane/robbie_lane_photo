9164e9719473d7511f3288bf2f5b245e39b25ac4
Robbie Lane <lane.robbie@gmail.com>
2015-12-31 16:00:25 -0600

    add pagination arrows to galleries

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,4 +1,14 @@
 module ApplicationHelper
+  def svg_tag filename, options={}
+    file = File.read(Rails.root.join('app', 'assets', 'images', filename))
+    doc = Nokogiri::HTML::DocumentFragment.parse file
+    svg = doc.at_css 'svg'
+    if options[:class].present?
+      svg['class'] = options[:class]
+    end
+    doc.to_html.html_safe
+  end
+
   def gallery_cover(gallery)
     if gallery.pictures.first
       gallery.pictures.first.image.url(:thumb)
