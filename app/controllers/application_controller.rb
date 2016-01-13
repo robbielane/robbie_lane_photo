f79e6c72c8c160a38d39125453bc1111e338e807
Robbie Lane <lane.robbie@gmail.com>
2016-01-12 18:33:20 -0700

    removed unnecessary set_cart method

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,7 +3,6 @@ class ApplicationController < ActionController::Base
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :null_session
   helper_method :current_user, :authenticate!
-  before_action :set_cart
 
   def current_user
     @user ||= User.find(session[:user_id]) if session[:user_id]
