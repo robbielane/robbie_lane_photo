f5b622106e4a629eba14740b649323438d2bed83
Robbie Lane <lane.robbie@gmail.com>
2016-01-13 20:59:29 -0700

    fix credential headers

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -13,13 +13,13 @@ class ApplicationController < ActionController::Base
   end
 
   before_filter :add_allow_credentials_headers
- -
- -  def add_allow_credentials_headers
- -    response.headers['Access-Control-Allow-Origin'] = request.headers['Origin'] || '*'
- -    response.headers['Access-Control-Allow-Credentials'] = 'true'
- -  end
- -
- -  def options
- -    head :status => 200, :'Access-Control-Allow-Headers' => 'accept, content-type'
- -  end
+
+   def add_allow_credentials_headers
+     response.headers['Access-Control-Allow-Origin'] = request.headers['Origin'] || '*'
+     response.headers['Access-Control-Allow-Credentials'] = 'true'
+   end
+
+   def options
+     head :status => 200, :'Access-Control-Allow-Headers' => 'accept, content-type'
+   end
 end
